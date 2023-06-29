39731433fda75480e1501a7a277df0d3647b1786
Don <tzy404270552@gmail.com>
2023-06-29 19:56:02 +0800

    :construction: update zsh history command search key bind

--- config/darwin.zsh
+++ config/darwin.zsh
@@ -6,3 +6,22 @@ bindkey 'å' autosuggest-accept
 # slove locale problem
 LC_CTYPE=en_US.UTF-8
 LC_ALL=en_US.UTF-8
+
+
+# arrow up/down searches in history if line is already started
+# 上下键根据已输入的命令匹配历史
+
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search
+bindkey "^[[B" down-line-or-beginning-search
+
+#autoload -Uz history-search-end
+#zle -N history-beginning-search-backward-end history-search-end
+#zle -N history-beginning-search-forward-end history-search-end
+#bindkey "$terminfo[kcuu1]" history-beginning-search-backward-end
+#bindkey "$terminfo[kcud1]" history-beginning-search-forward-end
+
+
--- config/linux.zsh
+++ config/linux.zsh
@@ -2,5 +2,13 @@
 # zplug "zsh-users/zsh-autosuggestions"
 # 绑定 alt + a 
 bindkey "^[a" autosuggest-accept
-# 绑定 option + a 
-bindkey 'å' autosuggest-accept
+
+# arrow up/down searches in history if line is already started
+# 上下键根据已输入的命令匹配历史
+autoload -Uz history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+bindkey "$terminfo[kcuu1]" history-beginning-search-backward-end
+bindkey "$terminfo[kcud1]" history-beginning-search-forward-end
+
+

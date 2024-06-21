02a7057b7cb520fbb85daf2e5f490ee51f728892
Don <tzy404270552@gmail.com>
2024-06-21 16:13:34 +0800

    :construction: add brew path to PATH

--- config/darwin.zsh
+++ config/darwin.zsh
@@ -11,3 +11,6 @@ zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
 bindkey "^[[A" up-line-or-beginning-search
 bindkey "^[[B" down-line-or-beginning-search
+
+
+export PATH=/opt/homebrew/bin:"$PATH"

40de1781c7c8e19974e61d7aaa3828de99c41e8a
Don <tzy404270552@gmail.com>
2024-06-28 13:53:22 +0800

    :bug: wrong conda provider directory

--- config/application.zsh
+++ config/application.zsh
@@ -5,24 +5,31 @@
 export EDITOR="vim"
 
 # * Conda
+if [[ -d "$HOME/mambaforge-pypy3" ]]; then
+    conda_provider=mambaforge-pypy3
+elif [[ -d "$HOME/miniforge3" ]]; then
+    conda_provider=miniforge3
+fi
+
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$("$HOME/mambaforge-pypy3/bin/conda" 'shell.zsh' 'hook' 2>/dev/null)"
+__conda_setup="$("$HOME/$conda_provider/bin/conda" 'shell.zsh' 'hook' 2>/dev/null)"
 if [ $? -eq 0 ]; then
 	eval "$__conda_setup"
 else
-	if [ -f "$HOME/mambaforge-pypy3/etc/profile.d/conda.sh" ]; then
-		. "$HOME/mambaforge-pypy3/etc/profile.d/conda.sh"
+	if [ -f "$HOME/$conda_provider/etc/profile.d/conda.sh" ]; then
+		. "$HOME/$conda_provider/etc/profile.d/conda.sh"
 	else
-		export PATH="$HOME/mambaforge-pypy3/bin:$PATH"
+		export PATH="$HOME/$conda_provider/bin:$PATH"
 	fi
 fi
 unset __conda_setup
 
-if [ -f "$HOME/mambaforge-pypy3/etc/profile.d/mamba.sh" ]; then
-	. "$HOME/mambaforge-pypy3/etc/profile.d/mamba.sh"
+if [ -f "$HOME/$conda_provider/etc/profile.d/mamba.sh" ]; then
+	. "$HOME/$conda_provider/etc/profile.d/mamba.sh"
 fi
 # <<< conda initialize <<<
+
 
 # * Golang
 export GOPATH=$HOME/go
